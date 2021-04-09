45a94646cab84f8cae8d9be53470f447d795ee57
Maxim Musiev <musiev_maksim@mail.ru>
2021-04-09 16:24:35 +0500

    Добавьте файлы проекта.

---
+++ WinAPI_Process_Lab/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+namespace WinAPI_Process_Lab
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.altoSlidingLabel1 = new AltoControls.AltoSlidingLabel();
+            this.VirtualMemorySize_SlidingLabel = new AltoControls.AltoSlidingLabel();
+            this.altoTextBox1 = new AltoControls.AltoTextBox();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.metroPanel1 = new MetroFramework.Controls.MetroPanel();
+            this.metroPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // altoSlidingLabel1
+            // 
+            this.altoSlidingLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.altoSlidingLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.altoSlidingLabel1.Location = new System.Drawing.Point(23, 63);
+            this.altoSlidingLabel1.Name = "altoSlidingLabel1";
+            this.altoSlidingLabel1.Size = new System.Drawing.Size(384, 15);
+            this.altoSlidingLabel1.Slide = false;
+            this.altoSlidingLabel1.TabIndex = 3;
+            this.altoSlidingLabel1.Text = "Количество зарезервированной виртуальной памяти:";
+            // 
+            // VirtualMemorySize_SlidingLabel
+            // 
+            this.VirtualMemorySize_SlidingLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.VirtualMemorySize_SlidingLabel.ForeColor = System.Drawing.Color.Blue;
+            this.VirtualMemorySize_SlidingLabel.Location = new System.Drawing.Point(403, 63);
+            this.VirtualMemorySize_SlidingLabel.Name = "VirtualMemorySize_SlidingLabel";
+            this.VirtualMemorySize_SlidingLabel.Size = new System.Drawing.Size(152, 15);
+            this.VirtualMemorySize_SlidingLabel.Slide = false;
+            this.VirtualMemorySize_SlidingLabel.TabIndex = 4;
+            // 
+            // altoTextBox1
+            // 
+            this.altoTextBox1.BackColor = System.Drawing.Color.Transparent;
+            this.altoTextBox1.Br = System.Drawing.Color.White;
+            this.altoTextBox1.Font = new System.Drawing.Font("Comic Sans MS", 11F);
+            this.altoTextBox1.ForeColor = System.Drawing.Color.DimGray;
+            this.altoTextBox1.Location = new System.Drawing.Point(3, 3);
+            this.altoTextBox1.Name = "altoTextBox1";
+            this.altoTextBox1.Size = new System.Drawing.Size(135, 33);
+            this.altoTextBox1.TabIndex = 5;
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Location = new System.Drawing.Point(133, 64);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(64, 33);
+            this.metroButton1.TabIndex = 6;
+            this.metroButton1.Text = "Read";
+            this.metroButton1.UseSelectable = true;
+            // 
+            // metroPanel1
+            // 
+            this.metroPanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.metroPanel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.metroPanel1.Controls.Add(this.metroButton1);
+            this.metroPanel1.Controls.Add(this.altoTextBox1);
+            this.metroPanel1.HorizontalScrollbarBarColor = true;
+            this.metroPanel1.HorizontalScrollbarHighlightOnWheel = false;
+            this.metroPanel1.HorizontalScrollbarSize = 10;
+            this.metroPanel1.Location = new System.Drawing.Point(23, 130);
+            this.metroPanel1.Name = "metroPanel1";
+            this.metroPanel1.Size = new System.Drawing.Size(200, 100);
+            this.metroPanel1.TabIndex = 7;
+            this.metroPanel1.VerticalScrollbarBarColor = true;
+            this.metroPanel1.VerticalScrollbarHighlightOnWheel = false;
+            this.metroPanel1.VerticalScrollbarSize = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.metroPanel1);
+            this.Controls.Add(this.VirtualMemorySize_SlidingLabel);
+            this.Controls.Add(this.altoSlidingLabel1);
+            this.Name = "Form1";
+            this.Text = "Write and Read";
+            this.metroPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private AltoControls.AltoSlidingLabel altoSlidingLabel1;
+        private AltoControls.AltoSlidingLabel VirtualMemorySize_SlidingLabel;
+        private AltoControls.AltoTextBox altoTextBox1;
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private MetroFramework.Controls.MetroPanel metroPanel1;
+    }
+}
+
---
+++ WinAPI_Process_Lab/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MetroFramework.Controls;
+namespace WinAPI_Process_Lab
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        Data data;
+        public Form1()
+        {
+            InitializeComponent();
+            data = new Data();
+            data.p = Process.GetCurrentProcess();
+            
+            ApiMethods.GetPhysicallyInstalledSystemMemory(out data.RAM_Size);
+            
+            VirtualMemorySize_SlidingLabel.Text = 
+                (data.p.VirtualMemorySize64 / (1024*1024)).ToString("0.0") + "/" + (data.RAM_Size / (1024)).ToString("0.0");
+        }
+
+
+
+
+
+    }
+    public class Data
+    {
+        public Process p;
+        public long RAM_Size;
+
+        StringBuilder ReadingValue;
+        StringBuilder WritingValue;
+
+    }
+
+    public class ApiMethods
+    {
+        [DllImport("kernel32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool GetPhysicallyInstalledSystemMemory(out long TotalMemoryInKilobytes);
+
+
+        [DllImport("kernel32.dll")]
+        public static extern bool ReadProcessMemory(IntPtr hProcess, long lpBaseAddress, byte[] lpBuffer, int dwSize, ref int lpNumberOfBytesRead);
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool WriteProcessMemory(IntPtr hProcess, long lpBaseAddress, byte[] lpBuffer, int dwSize, ref int lpNumberOfBytesWritten);
+    }
+}
